45ec2e55349a5b14f30d56301c8844ebc81ba964
danish <danish@gmail.com>
2022-06-04 12:10:45 +0530

    first commit

---
+++ agetracker/agetracker2/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
+def forgot(request):
+    return render(request,'forgot.html')    
+
+
